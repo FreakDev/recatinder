006323da67a38edb649c2bc28396a51ed78360ff
Mathias Desloges <freakdev@gmail.com>
2017-09-22 21:11:45 +0200

    fix buttons

--- src/modules/home/components/Home.js
+++ src/modules/home/components/Home.js
@@ -12,8 +12,8 @@ const HomeCmp = ({ onChats, onDogs }) => {
     return (
         <div className="App-home">
             <div className="background"></div>
-            <div onClick={ onChats } className="btn chats"><span className="figure"></span><br /><span className="label">Chats (fr)</span></div>
-            <div onClick={ onDogs } className="btn dogs"><span className="figure"></span><br /><span className="label">Dogs (en)</span></div>
+            <div onClick={ onChats } className="btn chats"><span className="figure">&nbsp;</span><br /><span className="label">Chats (fr)</span></div>
+            <div onClick={ onDogs } className="btn dogs"><span className="figure">&nbsp;</span><br /><span className="label">Dogs (en)</span></div>
             <div className="logo"></div>
         </div>
     )
